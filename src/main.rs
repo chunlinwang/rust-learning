7ba8d0666cafb88e755443c5dc94eb101fe04a08
chunlinwang <chunlinwang1983@gmail.com>
2020-10-19 22:54:04 +0200

    test tokio select

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,28 @@
+use tokio::sync::oneshot;
 use std::thread::sleep;
-use std::time::Duration;
-use chrono::Local;
-
-async fn hello(str: &str, delays: u64) {
-    let t1 = Local::now();
-    println!("start: {} ( {} )", str, t1.format("%Y-%m-%d %H:%M:%S").to_string());
-    sleep(Duration::from_secs(delays));
-    println!("{} - duration: {}", str, delays);
-    let t2 = Local::now();
-    println!("end: {} ( {} )", str, t2.format("%Y-%m-%d %H:%M:%S").to_string());
-}
-
-async fn test() {
-    tokio::spawn(async {
-        hello("task 1", 2).await;
-    });
-
-    tokio::spawn(async {
-        hello("task 2", 2).await;
-    });
-
-    tokio::spawn(async {
-        hello("task 3", 5).await;
-    });
-
-    tokio::spawn(async {
-        hello("task 4", 5).await;
-    });
-
-    tokio::spawn(async {
-        hello("task 5", 3).await;
-    });
-
-    tokio::spawn(async {
-        hello("task 6", 4).await;
-    });
-
-    tokio::spawn(async {
-        hello("task 7", 5).await;
-    });
-
-    tokio::spawn(async {
-        hello("task 8", 5).await;
-    });
-}
+use tokio::time::Duration;
 
 #[tokio::main]
 async fn main() {
-    test().await;
+    let (tx1, rx1) = oneshot::channel();
+    let (tx2, rx2) = oneshot::channel();
+
+    tokio::spawn(async {
+        sleep(Duration::from_secs(5));
+
+        let _ = tx1.send("one");
+    });
+
+    tokio::spawn(async {
+        let _ = tx2.send("two");
+    });
+
+    tokio::select! {
+        val = rx1 => {
+            println!("rx1 completed first with {:?}", val);
+        }
+        val = rx2 => {
+            println!("rx2 completed first with {:?}", val);
+        }
+    }
 }
